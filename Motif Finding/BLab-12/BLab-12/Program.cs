859b3ddf1951446d196cfbbea092bb7a4d68ef05
antkarasev76 <KarasevAnt24@mail.ru>
2018-12-21 15:56:11 +0300

    message

---
+++ Motif Finding/BLab-12/BLab-12/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLab_12
+{
+    class Program
+    {
+        static List<string> get_sp_words(string alpab, int len)
+        { 
+             List<string> sp_words = new List<string>((int)Math.Pow(4, len)); 
+             get_sp_words(ref sp_words, alpab, len, ""); 
+             return sp_words; 
+        } 
+
+         static void get_sp_words(ref List<string> sp_words, string alpab, int len, string p)
+         { 
+             if (len == 0) 
+             { 
+                 sp_words.Add(p); 
+             } 
+             else 
+                 foreach (char c in alpab) 
+                 { 
+                     get_sp_words(ref sp_words, alpab, len - 1, p + c); 
+                 } 
+         } 
+
+         static bool is_val_mut(string orig, string rep, int count_m)
+         { 
+             int len = orig.Length;
+             int mismat = 0;
+            
+             for (int i = 0; i<len; i++) 
+             { 
+                 if (orig[i] != rep[i]) 
+                 { 
+                     mismat++; 
+                 } 
+                 if (mismat > count_m) 
+                 { 
+                     return false; 
+                 } 
+             } 
+ 
+ 
+             return true; 
+         } 
+
+         static List<string> get_kd_mod(string km, int count_m)
+         { 
+             string alpab = "AGCT"; 
+             List<string> patterns = new List<string>(); 
+ 
+             foreach (var it in get_sp_words(alpab, km.Length)) 
+             { 
+                 if (is_val_mut(it.ToString(), km, count_m)) 
+                 { 
+                     patterns.Add(it.ToString()); 
+                 } 
+             } 
+
+             return patterns; 
+         } 
+ 
+ 
+         static void Main(string[] args)
+         { 
+             string[] tok = Console.ReadLine().Split(' '); 
+             int k = int.Parse(tok[0]); 
+             int d = int.Parse(tok[1]); 
+ 
+ 
+             string buffer = ""; 
+             while (true) 
+             { 
+                 string s = Console.ReadLine(); 
+                 if (string.IsNullOrEmpty(s)) 
+                     break; 
+ 
+                 buffer += s + ' '; 
+             } 
+ 
+ 
+             string[] dna_tab = buffer.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray(); 
+ 
+ 
+             List<string> patterns = new List<string>(); 
+ 
+ 
+             foreach (string str in dna_tab) 
+             { 
+                 int len = str.Length; 
+                 for (int i = 0; i < len - k + 1; i++) 
+                 { 
+                     string km = str.Substring(i, k); 
+ 
+                     foreach (string pattern in get_kd_mod(km, d)) 
+                     { 
+                         int count = 0;
+                        
+                         foreach (string subst in dna_tab) 
+                         { 
+                             for (int j = 0; j< len - k + 1; j++) 
+                             { 
+                                 if (is_val_mut(subst.Substring(j, k), pattern, d)) 
+                                 { 
+                                     count++; 
+                                     break; 
+                                 } 
+                             } 
+                         } 
+ 
+ 
+                         if (count == dna_tab.Length) 
+                         { 
+                             patterns.Add(pattern); 
+                         } 
+                     } 
+                 } 
+             } 
+ 
+             patterns = patterns.Distinct().ToList(); 
+ 
+             Console.WriteLine(string.Join(" ", patterns)); 
+             Console.ReadKey(); 
+         } 
+
+    }
+}
---
+++ Motif Finding/BLab-13/BLab-13/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLab_13
+{
+    class Program
+    {
+        static List<string> get_sp_words(string alpab, int len)
+        {
+            List<string> sp_words = new List<string>((int)Math.Pow(4, len));
+            get_sp_words(ref sp_words, alpab, len, "");
+            return sp_words;
+        }
+
+        static void get_sp_words(ref List<string> sp_words, string alpab, int len, string p)
+        {
+            if (len == 0)
+            {
+                sp_words.Add(p);
+            }
+            else
+                foreach (char c in alpab)
+                {
+                    get_sp_words(ref sp_words, alpab, len - 1, p + c);
+                }
+        }
+
+        static int c_disc(string a, string b)
+        { 
+             int count = 0; 
+             int len = a.Length; 
+ 
+             for (int i = 0; i < len; i++) 
+             { 
+                 if (a[i] != b[i]) 
+                 { 
+                     count++; 
+                 } 
+             } 
+ 
+             return count; 
+        }
+        
+        static int d(string pat, string text)
+        { 
+             int len = text.Length; 
+             int k = pat.Length; 
+ 
+             List<int> distance = new List<int>(); 
+ 
+ 
+             for (int i = 0; i < len - k + 1; i++) 
+             { 
+                 distance.Add(c_disc(pat, text.Substring(i, k))); 
+             } 
+ 
+             return distance.Min(); 
+        }
+        
+        static int d_dna_t(string pat, string[] dna)
+        { 
+             int sum = 0; 
+ 
+             foreach (string dnait in dna) 
+             { 
+                 sum += d(pat, dnait); 
+             } 
+ 
+             return sum; 
+        }
+        
+        static string MedianString(string[] dna, int k)
+        { 
+             int dist = int.MaxValue; 
+             string median = ""; 
+             List<string> allKm = get_sp_words("AGCT", k); 
+ 
+ 
+             foreach (string km in allKm) 
+             { 
+                 if (dist > d_dna_t(km, dna)) 
+                 { 
+                     dist = d_dna_t(km, dna); 
+                     median = km; 
+                 } 
+             } 
+
+             return median; 
+        } 
+
+        static void Main(string[] args)
+        { 
+             int k = int.Parse(Console.ReadLine()); 
+ 
+             string buffer = ""; 
+             while (true) 
+             { 
+                 string s = Console.ReadLine(); 
+                 if (string.IsNullOrEmpty(s)) 
+                     break; 
+ 
+                 buffer += s + ' '; 
+             } 
+ 
+ 
+             string[] dna = buffer.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); 
+      
+             Console.WriteLine(MedianString(dna, k)); 
+             Console.ReadKey(); 
+         } 
+    }
+}
